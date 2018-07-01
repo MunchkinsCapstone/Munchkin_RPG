7628481f129d244075757ce1b8d0abf6820e6dde
Graham Bass <35979137+bassgt95@users.noreply.github.com>
2018-07-01 15:20:57 -0400

    Revert "Sockets"

--- client/socket.js
+++ client/socket.js
@@ -1,21 +1,9 @@
-//CLIENT SIDE SOCKET
-
 import io from 'socket.io-client'
 
 const socket = io(window.location.origin)
 
 socket.on('connect', () => {
   console.log('Connected!')
-  // socket.on('get rooms', (rooms) => {
-  //   console.log('hearing from socket client', rooms)
-  // })
-
-
-  // socket.on('roomAdded', payload => {})
-
-  // socket.on('button2', str => {
-  //   console.log('here is that string', str)
-  // })
 })
 
 export default socket
